b4133c22d4dc8b5b0c58db8f60809e66d44ac45c
exectlyyy <ivanyburov@gmail.com>
2023-10-18 22:47:07 +0300

    v0.2
    bug fix

--- venv/main.py
+++ venv/main.py
@@ -11,7 +11,7 @@ def udarenie(slovo):
     var = []
     slovo = [i for i in slovo]
     for i in range(len(slovo)):
-        if slovo[i] in 'уеэоаыяию':
+        if slovo[i] in 'уеэоаыяиюё':
             new = [j for j in slovo]
             new[i] = slovo[i].upper() 
             var.append(''.join(new))
@@ -20,13 +20,24 @@ def udarenie(slovo):
 @bot.message_handler(commands=['start'])
 
 def start(message):
-    bot.send_message(message.from_user.id, 'Привет')
-
+    bot.send_message(message.from_user.id, 'Привет, этот бот поможет выучить ударения ЕГЭ, напиши /help для полного списка команд')
+@bot.message_handler(commands=['help'])   
+def help(message):
+      bot.send_message(message.from_user.id, '''/go - начать тест
+/theory - теория
+/stop - закончить тест''')
 
 user_data = []
 actual_word = ''
+is_first = {}
 
-@bot.message_handler(commands=['go'])
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    global actual_word
+    actual_word = ''
+    markup = types.ReplyKeyboardRemove(selective=False)
+
+@bot.message_handler(commands=['go', 'next'])
 def test(message):
     global actual_word
     with open(r'Data/u.txt') as f:
@@ -43,10 +54,15 @@ def test(message):
         markup = types.ReplyKeyboardRemove(selective=False)
         var = []
 
+@bot.message_handler(commands=['theory'])
+def theory(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(message.from_user.id, f'''Теория и лайфхаки ударения ЕГЭ''', reply_markup=markup)
+
 
 @bot.message_handler(content_types=['text'])
 def main(message):
-    global actual_word, user_data
+    global actual_word, user_data, is_first
     file = f'''{message.from_user.id}.txt'''
     folder = '/Users/ivanyburov/Documents/GitHub/XF20874892SHINZOU/User_Data'
     path = os.path.join(folder, file)
@@ -55,14 +71,19 @@ def main(message):
         file.close()
         os.rename('/Users/ivanyburov/Documents/GitHub/XF20874892SHINZOU/User_Data/file.txt',
                   f'''/Users/ivanyburov/Documents/GitHub/XF20874892SHINZOU/User_Data/{message.from_user.id}''')
-    if message.from_user.id not in user_data:
+    if False:
         user_data.append(message.from_user.id)
-        bot.send_message(message.from_user.id, 'Стартуем') #Дописать что бы кнопка на старт была    
+        bot.send_message(message.from_user.id, 'Привет, этот бот поможет тебе выучить задание номер 4 ЕГЭ по русскому языку! Нажми /start, чтобы продолжить.')    
     else:
         if message.text == actual_word:
-            bot.send_message(message.from_user.id, 'верно')
+            markup = types.ReplyKeyboardMarkup()
+            markup.add('/next')
+            bot.send_message(message.from_user.id, 'Верно', reply_markup=markup)
         else:
-            bot.send_message(message.from_user.id, 'ты даун')
+            markup = types.ReplyKeyboardMarkup()
+            markup.add('/next')
+            bot.send_message(message.from_user.id, f'Неправильное ударение, верный вариант: {actual_word}', reply_markup=markup)
+            
     print(user_data)
     
 bot.infinity_polling(1080)
